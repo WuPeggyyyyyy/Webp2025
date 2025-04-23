113adb47bb196e9aa556218e59b2dd7d7a34ab88
WuPeggyyyyyy <144549599+WuPeggyyyyyy@users.noreply.github.com>
2025-04-23 16:19:55 +0800

    Add files via upload

---
+++ lab11/src/App.js
@@ -0,0 +1,44 @@
+import logo from './logo.svg';
+import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+
+
+
+  const changeText=(event)=> {
+    console.log(event.target)
+    event.target.innerText = event.target.innerText + "被點了";
+  }
+
+  const styleArgument = { fontSize: '100px', color: 'red' };
+
+  function App() {
+    return (
+      <div className="App">
+        <h1 style = { styleArgument } onClick={changeText}> hello CGU!!
+        </h1>
+      </div>
+    );
+  }
+
+ export default App;
